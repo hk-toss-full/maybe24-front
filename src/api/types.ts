26e7d9f18ce997ca636c5c94337a21d4a555d4ad
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-28 00:57:19 +0900

    feat: What's Hot 섹션 임시 구현

---
+++ src/api/types.ts
@@ -0,0 +1,20 @@
+interface Round {
+  roundId: string;
+  totalCount: string;
+  date: string;
+  price: number;
+}
+
+interface Discount {
+  type: string;
+  rate: number;
+}
+
+// not used
+interface Product {
+  title: string;
+  location: string;
+  thumbnail: string;
+  discount?: Discount;
+  roundList: Round[];
+}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,32 @@
 import Banner from "../components/landing/Banner";
+import WhatsHot from "../components/landing/WhatsHot";
 
 const LandingPage = () => {
   return (
     <div>
       <Banner />
+      {/** WHAT'S HOT */}
+      <WhatsHot />
+      {/** TICKET OPEN */}
+      <></>
+      {/** SIDE BANNER */}
+      <></>
+      {/** RANKING */}
+      <></>
+      {/** etc */}
+      <></>
+      {/** etc (video) */}
+      <></>
+      {/** MUSICAL & PLAY */}
+      <></>
+      {/** CONCERT & CLASSIC */}
+      <></>
+      {/** EXHIBITION & KIDS */}
+      <></>
+      {/** REVIEW & INTERVIEW */}
+      <></>
+      {/** footer */}
+      <></>
     </div>
   );
 };
