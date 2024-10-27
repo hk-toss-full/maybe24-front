2b673273c3317216b7adc83c9e4d6da7d453119d
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-28 02:51:08 +0900

    fix: Ranking 섹션 CSS 깨짐 수정

--- src/components/landing/RankingItem.tsx
+++ src/components/landing/RankingItem.tsx
@@ -14,14 +14,14 @@ interface RankingItemProps {
 const RankingItem = (props: RankingItemProps) => {
   const { category, title, order, location, thumbnail, roundList } = props;
   return (
-    <div className="h-80 w-92 group cursor-pointer flex-1">
-      <div className="relative h-full ">
-        <div className="bg-black/50 absolute w-full h-full top-0 z-10" />
-        <div className="h-full w-full overflow-hidden">
+    <div className="h-[428px] w-92 group cursor-pointer flex-1">
+      <div className="relative h-80 ">
+        <div className="bg-black/50 absolute w-full h-80 top-0 z-10" />
+        <div className="h-80 w-full overflow-hidden">
           <img
             src={thumbnail}
-            alt={`${category}의 랭킹 ${order}위 썸네일`}
-            className="w-full blur-sm object-cover group-hover:scale-110 transition-all duration-300"
+            alt={`${category}의 랭킹 ${order}위 썸네일 배경`}
+            className="h-80 w-full blur-sm object-cover group-hover:scale-110 transition-all duration-300"
           />
         </div>
         <img
@@ -29,7 +29,7 @@ const RankingItem = (props: RankingItemProps) => {
           alt={`${category}의 랭킹 ${order}위 썸네일`}
           className="absolute h-72 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
         />
-        <div className="absolute -bottom-3 left-4 w-12 h-12 z-30 bg-[#ec7d2c] text-white flex justify-center items-center">
+        <div className="absolute -bottom-3 left-4 w-12 h-12 z-30 bg-[#ec7d2c] text-white flex justify-center items-center text-[18px]">
           {order}위
         </div>
       </div>
