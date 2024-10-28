89190eef0d412816aff3cf7c15ea0cd296268652
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-29 04:00:11 +0900

    feat: logout 기능 구현

--- src/components/common/NavHeader.tsx
+++ src/components/common/NavHeader.tsx
@@ -1,9 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PersonPNG from "../../assets/person.png";
 import { PATH } from "../../utils/paths";
 
 const NavHeader = () => {
+  const navigate = useNavigate();
+
   const isLoggedIn = localStorage.getItem("token") !== null;
+
+  const onClick = () => {
+    if (isLoggedIn) {
+      const answer = confirm("로그아웃하시겠습니까?");
+      if (answer) {
+        localStorage.removeItem("token");
+        navigate(PATH.LOGIN);
+      }
+    } else {
+      navigate(PATH.LOGIN);
+    }
+  };
 
   return (
     <div className="absolute top-0 left-0 z-50 w-full p-8 flex justify-between">
@@ -11,9 +25,9 @@ const NavHeader = () => {
         src="http://tkfile.yes24.com/imgNew/common/pf-logoN-w.png"
         className="h-8"
       />
-      <Link to={isLoggedIn ? "" : PATH.LOGIN}>
+      <button onClick={onClick}>
         <img src={PersonPNG} className="w-6 h-6 text-white" />
-      </Link>
+      </button>
     </div>
   );
 };
