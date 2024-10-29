35d5a70e1c6f5329e170dae006652e87f2f3d789
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-29 12:58:13 +0900

    fix: 이미지 깨짐 수정 및 폴더 구조 변경

--- src/components/reviews/Add.tsx
+++ src/components/reviews/Add.tsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
-import productData from "../../data/product.json"
-import {getReviewList, addReview, updateReview } from "../api/ReviewApi"
+import productData from "../../data/product.json";
+import {
+  getReviewList,
+  addReview,
+  updateReview,
+} from "../../api/query/ReviewApi";
 
-interface productData{
+interface productData {
   productId: number;
   board: any;
-  setBoard: (board:any)=>void;
-  addPost: ()=>void;
-  updatePost: (reviewId:number)=>void;
+  setBoard: (board: any) => void;
+  addPost: () => void;
+  updatePost: (reviewId: number) => void;
 }
 
-const Add = ({ board, setBoard, addPost, updatePost }:productData) => {
-
-
+const Add = ({ board, setBoard, addPost, updatePost }: productData) => {
   const handleInput = (e) => {
     const { name, value } = e.target;
     setBoard({ ...board, [name]: value });
   };
 
-
   return (
     <div>
       <div className="flex flex-col align-center mt-[10px]">
-      <input
+        <input
           className="min-h-[25px] p-[10px] rounded"
           type="text"
           value={board.author}
@@ -30,13 +31,22 @@ const Add = ({ board, setBoard, addPost, updatePost }:productData) => {
           onChange={handleInput}
           placeholder="작성자"
         />
-        <hr/>
+        <hr />
         <div className="flex flex-row cursor-pointer text-[30px] m-[10px] -translate-y-1">
-          {[...Array(board.rating)].map((_, i)=>(
-            <div key={i} onClick={() => setBoard({...board, rating:i + 1})}>★</div>
+          {[...Array(board.rating)].map((_, i) => (
+            <div key={i} onClick={() => setBoard({ ...board, rating: i + 1 })}>
+              ★
+            </div>
           ))}
           {[...Array(5 - board.rating)].map((_, i) => (
-            <div key={i} onClick={() => setBoard({...board, rating:board.rating + i + 1})}>☆</div>
+            <div
+              key={i}
+              onClick={() =>
+                setBoard({ ...board, rating: board.rating + i + 1 })
+              }
+            >
+              ☆
+            </div>
           ))}
         </div>
         <hr />
@@ -55,11 +65,10 @@ const Add = ({ board, setBoard, addPost, updatePost }:productData) => {
           >
             ADD
           </button>
-          <button onClick={()=>updatePost(board.reviewId)}>UPDATE</button>
+          <button onClick={() => updatePost(board.reviewId)}>UPDATE</button>
         </div>
       </div>
-      <div className="mt-[30px]">
-      </div>
+      <div className="mt-[30px]"></div>
     </div>
   );
 };
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,34 +1,39 @@
 import Details from "../../components/details/Details";
 // import productData from "../../data/product.json"
 import { useLocation } from "react-router-dom";
-import { GET_PRODUCT_BY_ID } from "../../components/graphql/getProductById";
-import { GET_DISCOUNT_BY_PRODUCT_ID } from "../../components/graphql/getDiscountByProductId";
+import { GET_PRODUCT_BY_ID } from "../../api/query/getProductById";
+import { GET_DISCOUNT_BY_PRODUCT_ID } from "../../api/query/getDiscountByProductId";
 import { useQuery } from "@apollo/client";
-import { GET_ROUND_BY_ID } from "../../components/graphql/getRoundByProductId";
-
-
+import { GET_ROUND_BY_ID } from "../../api/query/getRoundByProductId";
 
 const DetailsPage = () => {
   const location = useLocation();
-  const productId = location.pathname.split('/')[2];
+  const productId = location.pathname.split("/")[2];
   // const [productId, setProductId] = useState();
 
   // const [productData, setProductData] = useState();
   // const [discountData, setDiscountData] = useState();
 
-  const { loading:pLoading,  data:productData } = useQuery(GET_PRODUCT_BY_ID, {variables:{productId:productId}});
+  const { loading: pLoading, data: productData } = useQuery(GET_PRODUCT_BY_ID, {
+    variables: { productId: productId },
+  });
   // const { loading:dLoading, data:discountData } = useQuery(GET_DISCOUNT_BY_PRODUCT_ID, {variables: {productId:productId}});
 
-  const {data:roundData} = useQuery
-  (GET_ROUND_BY_ID, {variables:{productId}});
+  const { data: roundData } = useQuery(GET_ROUND_BY_ID, {
+    variables: { productId },
+  });
 
   return (
-      <div>
-        {!pLoading&&
-        <Details productId={Number(productId)} productData={productData?.getProductById} roundData= {roundData.getRoundByProductId}/>}
-      </div>
-  )
-}
+    <div>
+      {!pLoading && (
+        <Details
+          productId={Number(productId)}
+          productData={productData?.getProductById}
+          roundData={roundData.getRoundByProductId}
+        />
+      )}
+    </div>
+  );
+};
 
 export default DetailsPage;
-
