6f3eab899fcfe1aa8494f46456c7248d55034964
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-29 13:04:30 +0900

    fix: review api 요청 port 수정

--- src/api/query/ReviewApi.tsx
+++ src/api/query/ReviewApi.tsx
@@ -1,35 +1,41 @@
-import axios from "axios";
+import { axiosInstance } from "../axios";
 
-export const getReviewList = async(productId: number) => {
-  const {data} = await axios({
+export const getReviewList = async (productId: number) => {
+  const { data } = await axiosInstance({
     method: "GET",
-    url: `http://localhost:9000/reviews/${productId}`
+    url: `/reviews/${productId}`,
   });
   return data;
-} 
+};
 
-export const addReview = async( {productId ,author, content, rating}) => {
-  const {data} = await axios({
+export const addReview = async ({ productId, author, content, rating }) => {
+  const { data } = await axiosInstance({
     method: "POST",
-    url: `http://localhost:9000/reviews/${productId}`,
-    data: {productId, author, content, rating}
+    url: `/reviews/${productId}`,
+    data: { productId, author, content, rating },
   });
   return data;
-} 
+};
 
-export const updateReview = async({reviewId, productId, author, content, rating}) => {
-  const {data} = await axios({
+export const updateReview = async ({
+  reviewId,
+  productId,
+  author,
+  content,
+  rating,
+}) => {
+  const { data } = await axiosInstance({
     method: "PUT",
-    url: `http://localhost:9000/reviews/${reviewId}`,
-    data: { productId, author, content, rating}
+    url: `/reviews/${reviewId}`,
+    data: { productId, author, content, rating },
   });
   return data;
-} 
+};
 
-export const deleteReview = async(reviewId: number) => {
-  const {data} = await axios({
+export const deleteReview = async (reviewId: number) => {
+  const { data } = await axiosInstance({
     method: "DELETE",
-    url: `http://localhost:9000/reviews/${reviewId}`
+    url: `/reviews/${reviewId}`,
   });
   return data;
-} 
+};
